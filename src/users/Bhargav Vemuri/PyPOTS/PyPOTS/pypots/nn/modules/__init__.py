1cdb1f95029bf8ffc529e325cb99bcf60f6523d7
minjune <minjune@i.inc>
2024-09-22 17:18:03 -0400

    solve err

--- src/users/Bhargav Vemuri/PyPOTS/PyPOTS/pypots/nn/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Frequently-used modules like self-attention modules of vanilla Transformer are put in this package.
-"""
-
-# Created by Wenjie Du <wenjay.du@gmail.com>
-# License: BSD-3-Clause
--- src/users/Jun Wang/PyGrinder/PyGrinder/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Test cases for PyGrinder.
-"""
-
-# Created by Wenjie Du <wenjay.du@gmail.com>
-# License: GLP-v3
